f6e076a99f59d2180445b18e98382d03c6aad821
Will Hudgins <willhudgins1@gmail.com>
2018-01-29 14:59:24 -0500

    API call to Dark Sky made

--- App.js
+++ App.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import config from './config.json';
 
 export default class App extends React.Component {
+
+  config = {};
+
+  constructor(props) {
+    super(props);
+    this.state = {currentTemp: null};
+
+    this.config = config;
+
+    this.fetchData();
+  }
+
   render() {
+    
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>{Math.round(this.state.currentTemp)}</Text>
+        
       </View>
     );
+  }
+
+
+  fetchData() {
+    var urlBase = 'https://api.darksky.net/forecast/'
+    var location = '/40.769,-73.957';
+    var fullUrl = urlBase + config.apiKey + location;
+    return fetch(fullUrl)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson.currently.apparentTemperature);
+      //this.state.currentTemp = responseJson.currently.apparentTemperature;
+
+      this.setState(previousState => {
+        return { currentTemp: responseJson.currently.apparentTemperature };
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      return "Error";
+    });
   }
 }
 
